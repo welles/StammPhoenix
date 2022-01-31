5135c02cf2d67207547fafe241adfe6c0ec07652
Nico Welles <nico@welles.io>
2022-01-31 21:46:39 +0100

    Add initial files from old repository

---
+++ StammPhoenix.Web/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿/*
+* Site Specific JS
+*/
+$(document).ready(function () {
+    const backgroundColors = ['#8D8741', '#659DBD', '#DAAD86', '#BC986A', '#553D67', '#E27D60'];
+    const backgroundColor = backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
+
+    $('body').css('background-color', backgroundColor);
+});
