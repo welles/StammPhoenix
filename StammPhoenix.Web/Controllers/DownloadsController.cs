e547a26aa08a0cd2087bcd8194964eaa8ddcce97
Nico Welles <nico@welles.io>
2022-02-06 04:53:56 +0100

    Add Downloads page and files

---
+++ StammPhoenix.Web/Controllers/DownloadsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StammPhoenix.Web.Controllers
+{
+    public class DownloadsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
