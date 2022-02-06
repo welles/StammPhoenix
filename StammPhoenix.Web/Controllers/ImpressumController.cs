057d88ec05f7b41af435045351da935752b660ae
Nico Welles <nico@welles.io>
2022-02-06 06:16:47 +0100

    Add Impressum page

---
+++ StammPhoenix.Web/Controllers/ImpressumController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StammPhoenix.Web.Controllers
+{
+    public class ImpressumController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
