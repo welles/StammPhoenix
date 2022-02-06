1a6143b960ec10b699c782b9b9fa3ea6b571a7ef
Nico Welles <nico@welles.io>
2022-02-06 04:30:31 +0100

    Add Stufen pages

---
+++ StammPhoenix.Web/Controllers/StufeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StammPhoenix.Web.Controllers
+{
+    public class StufeController : Controller
+    {
+        public IActionResult Woelflinge()
+        {
+            return View("~/Views/Stufe/Woelflinge.cshtml");
+        }
+        public IActionResult Jungpfadfinder()
+        {
+            return View();
+        }
+        public IActionResult Pfadfinder()
+        {
+            return View();
+        }
+        public IActionResult Rover()
+        {
+            return View();
+        }
+    }
+}
