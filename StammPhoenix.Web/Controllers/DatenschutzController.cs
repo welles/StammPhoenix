4d72ccc4d1ae9ad7d09f695d6a18c4cc74413e01
Nico Welles <nico@welles.io>
2022-02-06 05:56:35 +0100

    Add Datenschutz page

---
+++ StammPhoenix.Web/Controllers/DatenschutzController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StammPhoenix.Web.Controllers
+{
+    public class DatenschutzController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
