3e38d7a409d17433828540ede5167df4f998cd48
Nico Welles <nico@welles.io>
2022-03-18 10:53:06 +0100

    Add PageContact table

---
+++ StammPhoenix.Persistence/Models/PageContact.cs
@@ -0,0 +1,9 @@
+﻿#pragma warning disable CS8618 // Disable nullable warning
+namespace StammPhoenix.Persistence.Models;
+
+public class PageContact
+{
+    public string Name { get; set; }
+
+    public string PhoneNumber { get; set; }
+}
