bb3afdd2f51ee94459a126ca39e815abd53be41d
Nico Welles <nico@welles.io>
2022-03-18 11:00:05 +0100

    Add Unit Test project

---
+++ StammPhoenix.Test/Core/WebMapperTests.cs
@@ -0,0 +1,17 @@
+﻿using FakeItEasy;
+using NUnit.Framework;
+using StammPhoenix.Web.Core;
+
+namespace StammPhoenix.Test.Core;
+
+[TestFixture]
+public static class WebMapperTests
+{
+    [Test]
+    public static void ValidateConfiguration()
+    {
+        var mapper = WebMapper.Create(A.Fake<IServiceProvider>());
+
+        mapper.ConfigurationProvider.AssertConfigurationIsValid();
+    }
+}
