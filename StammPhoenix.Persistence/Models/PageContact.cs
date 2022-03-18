cc40e5b02178ac7dabaa33e8b3c981681b542a5f
Nico Welles <nico@welles.io>
2022-03-18 11:06:37 +0100

    Add ID property to PageContact

--- StammPhoenix.Persistence/Models/PageContact.cs
+++ StammPhoenix.Persistence/Models/PageContact.cs
@@ -3,6 +3,8 @@ namespace StammPhoenix.Persistence.Models;
 
 public class PageContact
 {
+    public string Id { get; set; }
+
     public string Name { get; set; }
 
     public string PhoneNumber { get; set; }
