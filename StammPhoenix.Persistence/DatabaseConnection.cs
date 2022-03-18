5f6ff6b9c4fd3a9bfad8fe6a60da77748a2e94c3
Nico Welles <nico@welles.io>
2022-03-18 10:10:00 +0100

    Read database connection from Docker env file during development

--- StammPhoenix.Persistence/DatabaseConnection.cs
+++ StammPhoenix.Persistence/DatabaseConnection.cs
@@ -2,18 +2,39 @@
 {
     public class DatabaseConnection
     {
+        public const string HostParameter = "DATABASE_HOST";
+        public const string PortParameter = "DATABASE_PORT";
+        public const string DatabaseParameter = "DATABASE_NAME";
+        public const string UsernameParameter = "DATABASE_USERNAME";
+        public const string PasswordParameter = "DATABASE_PASSWORD";
+
         public DatabaseConnection()
         {
-            this.Host = Environment.GetEnvironmentVariable("DATABASE_HOST");
-            this.Port = int.Parse(Environment.GetEnvironmentVariable("DATABASE_PORT"));
-            this.Database = Environment.GetEnvironmentVariable("DATABASE_NAME");
-            this.Username = Environment.GetEnvironmentVariable("DATABASE_USERNAME");
-            this.Password = Environment.GetEnvironmentVariable("DATABASE_PASSWORD");
+            if (File.Exists("Dockerfile.env"))
+            {
+                var values = File.ReadAllLines("Dockerfile.env")
+                    .Select(x => x.Split("="))
+                    .ToDictionary(x => x[0], x => x[1]);
+
+                this.Host = values[HostParameter];
+                this.Port = values[PortParameter];
+                this.Database = values[DatabaseParameter];
+                this.Username = values[UsernameParameter];
+                this.Password = values[PasswordParameter];
+
+                return;
+            }
+
+            this.Host = Environment.GetEnvironmentVariable(HostParameter);
+            this.Port = Environment.GetEnvironmentVariable(PortParameter);
+            this.Database = Environment.GetEnvironmentVariable(DatabaseParameter);
+            this.Username = Environment.GetEnvironmentVariable(UsernameParameter);
+            this.Password = Environment.GetEnvironmentVariable(PasswordParameter);
         }
 
         public string Host { get; }
 
-        public int Port { get; }
+        public string Port { get; }
 
         public string Database { get; }
 
