1d8784c0ffa33c1b8940d61a1c313a9fc55f9fca
Nico Welles <nico@welles.io>
2022-03-18 11:07:11 +0100

    Add PageContact DB migration

---
+++ StammPhoenix.Persistence/Migrations/20220318100646_AddPageContact.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StammPhoenix.Persistence.Migrations
+{
+    public partial class AddPageContact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PageContacts",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PageContacts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PageContacts");
+        }
+    }
+}
