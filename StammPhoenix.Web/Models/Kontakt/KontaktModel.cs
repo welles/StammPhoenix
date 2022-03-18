e065a5fc9b1ad852307a45c24e6f0ff5b041f6f5
Nico Welles <nico@welles.io>
2022-03-18 10:54:08 +0100

    Add models for Kontakt page

---
+++ StammPhoenix.Web/Models/Kontakt/KontaktModel.cs
@@ -0,0 +1,14 @@
+﻿namespace StammPhoenix.Web.Models.Kontakt;
+
+public class KontaktModel
+{
+    public KontaktModel(string name, string phoneNumber)
+    {
+        this.Name = name;
+        this.PhoneNumber = phoneNumber;
+    }
+
+    public string Name { get; }
+
+    public string PhoneNumber { get; }
+}
---
+++ StammPhoenix.Web/Models/Kontakt/KontaktViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StammPhoenix.Web.Models.Kontakt;
+
+public class KontaktViewModel
+{
+    public KontaktViewModel(KontaktModel[] contacts)
+    {
+        this.Contacts = contacts;
+    }
+
+    public KontaktModel[] Contacts { get; }
+}
