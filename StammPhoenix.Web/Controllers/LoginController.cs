e01eae1e3fdd1daf38ac4f45f80af6a88f24db8f
Nico Welles <nico@welles.io>
2022-03-19 14:09:33 +0100

    Add basic login page

--- StammPhoenix.Web/Controllers/LoginController.cs
+++ StammPhoenix.Web/Controllers/LoginController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StammPhoenix.Web.Models.Login;
 
 namespace StammPhoenix.Web.Controllers;
 
 public class LoginController : Controller
 {
+    [HttpGet]
     public IActionResult Index()
+    {
+        return this.View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(LoginModel form)
     {
         return this.View();
     }
---
+++ StammPhoenix.Web/Models/Login/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace StammPhoenix.Web.Models.Login;
+
+public class LoginModel
+{
+    public string? Username { get; set; }
+
+    public string? Password { get; set; }
+}
