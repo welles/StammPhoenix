4548d784338ea8a2fb4e79010532b09c2008facd
Nico Welles <nico@welles.io>
2022-03-22 02:54:52 +0100

    Refactor login controller

--- StammPhoenix.Web/Controllers/LoginController.cs
+++ StammPhoenix.Web/Controllers/LoginController.cs
@@ -1,10 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using StammPhoenix.Persistence;
+using StammPhoenix.Persistence.Models;
+using StammPhoenix.Web.Core;
 using StammPhoenix.Web.Models.Login;
 
 namespace StammPhoenix.Web.Controllers;
 
 public class LoginController : Controller
 {
+    private IDatabaseContext DatabaseContext { get; }
+
+    private IPasswordHasher PasswordHasher { get; }
+
+    public LoginController(IDatabaseContext databaseContext, IPasswordHasher passwordHasher)
+    {
+        this.DatabaseContext = databaseContext;
+        this.PasswordHasher = passwordHasher;
+    }
+
     [HttpGet]
     public IActionResult Index()
     {
@@ -12,8 +28,68 @@ public class LoginController : Controller
     }
 
     [HttpPost]
-    public IActionResult Index(LoginModel form)
+    public async Task<IActionResult> Index(LoginModel form)
     {
-        return this.View();
+        if (string.IsNullOrWhiteSpace(form.Username) || string.IsNullOrWhiteSpace(form.Password))
+        {
+            throw new NotImplementedException();
+        }
+
+        var user = await this.DatabaseContext.FindUserById(form.Username);
+
+        if (user == null)
+        {
+            throw new NotImplementedException();
+        }
+
+        if (user.IsLocked)
+        {
+            throw new NotImplementedException();
+        }
+
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.Id),
+            new Claim(ClaimTypes.GivenName, user.Name),
+            new Claim(ClaimTypes.Role, user.Role.ToString())
+        };
+
+        if (user.NeedPasswordChange)
+        {
+            claims.Add(new Claim(nameof(LoginUser.NeedPasswordChange), "true"));
+        }
+
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+        var authProperties = new AuthenticationProperties
+        {
+            //AllowRefresh = <bool>,
+            // Refreshing the authentication session should be allowed.
+
+            //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+            // The time at which the authentication ticket expires. A
+            // value set here overrides the ExpireTimeSpan option of
+            // CookieAuthenticationOptions set with AddCookie.
+
+            //IsPersistent = true,
+            // Whether the authentication session is persisted across
+            // multiple requests. When used with cookies, controls
+            // whether the cookie's lifetime is absolute (matching the
+            // lifetime of the authentication ticket) or session-based.
+
+            //IssuedUtc = <DateTimeOffset>,
+            // The time at which the authentication ticket was issued.
+
+            //RedirectUri = <string>
+            // The full path or absolute URI to be used as an http
+            // redirect response value.
+        };
+
+        await HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(claimsIdentity),
+            authProperties);
+
+        return this.RedirectToAction("Index", "Home");
     }
 }
