b3a43fbeddc2353ed332396fa128464662e6cfc8
Nico Welles <nico@welles.io>
2022-03-22 18:53:11 +0100

    Require login for Leiter area

--- StammPhoenix.Web/Areas/Leiter/Controllers/HomeController.cs
+++ StammPhoenix.Web/Areas/Leiter/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace StammPhoenix.Web.Areas.Leiter.Controllers
 {
+    [Authorize]
     [Area("Leiter")]
     public class HomeController : Controller
     {
