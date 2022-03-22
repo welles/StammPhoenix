daf6f8514eb240b1d6147fe4aa14c8201c845016
Nico Welles <nico@welles.io>
2022-03-22 02:35:14 +0100

    Remove Authentication project

--- StammPhoenix.Authentication/Models/ApplicationRole.cs
+++
@@ -1,9 +0,0 @@
-﻿#pragma warning disable CS8618 // Disable nullable warning
-namespace StammPhoenix.Authentication.Models;
-
-public class ApplicationRole
-{
-    public string Guid { get; set; }
-
-    public string Name { get; set; }
-}
