105b28a3b0e719455f6a3d340c523e6a8b07112f
Nico Welles <nico@welles.io>
2022-03-22 02:55:22 +0100

    Show name in layout

---
+++ StammPhoenix.Web/Extensions/IdentityExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace StammPhoenix.Web.Extensions;
+
+public static class IdentityExtensions
+{
+    public static string GetGivenName(this IIdentity? identity)
+    {
+        var claimsIdentity = (ClaimsIdentity) identity;
+
+        return claimsIdentity.Claims.Single(x => x.Type == ClaimTypes.GivenName).Value;
+    }
+}
