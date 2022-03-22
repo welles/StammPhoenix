8005bbd911b6de41ca8f279a60ce07241b42b6fe
Nico Welles <nico@welles.io>
2022-03-22 02:38:26 +0100

    Add InitialSetup middleware and page

---
+++ StammPhoenix.Web/Controllers/InitialSetupController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StammPhoenix.Persistence;
+using StammPhoenix.Persistence.Constants;
+using StammPhoenix.Persistence.Models;
+using StammPhoenix.Web.Core;
+using StammPhoenix.Web.Models.InitialSetup;
+
+namespace StammPhoenix.Web.Controllers;
+
+public class InitialSetupController : Controller
+{
+    private IDatabaseContext DatabaseContext { get; }
+
+    private IPasswordHasher PasswordHasher { get; }
+
+    public InitialSetupController(IDatabaseContext databaseContext, IPasswordHasher passwordHasher)
+    {
+        this.DatabaseContext = databaseContext;
+        this.PasswordHasher = passwordHasher;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return this.View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(InitialSetupModel form)
+    {
+        if (string.IsNullOrWhiteSpace(form.Username)
+            || string.IsNullOrWhiteSpace(form.DisplayName)
+            || string.IsNullOrWhiteSpace(form.Password)
+            || string.IsNullOrWhiteSpace(form.RepeatPassword))
+        {
+            throw new NotImplementedException();
+        }
+
+        var hashedPassword = this.PasswordHasher.HashPassword(form.Password);
+
+        var user = new LoginUser
+        {
+            Id = form.Username,
+            Name = form.DisplayName,
+            PasswordHash = hashedPassword,
+            IsLocked = false,
+            NeedPasswordChange = false
+        };
+
+        await this.DatabaseContext.CreateUser(user);
+
+        await this.DatabaseContext.UpdateSetting(SettingNames.InitialSetupComplete, true);
+
+        return this.RedirectToAction("Index", "Login");
+    }
+}
---
+++ StammPhoenix.Web/Models/InitialSetup/InitialSetupModel.cs
@@ -0,0 +1,12 @@
+﻿namespace StammPhoenix.Web.Models.InitialSetup;
+
+public class InitialSetupModel
+{
+    public string? Username { get; set; }
+
+    public string? DisplayName { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? RepeatPassword { get; set; }
+}
