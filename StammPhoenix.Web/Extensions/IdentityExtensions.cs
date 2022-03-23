1820f963c998a3fd136d43243beb5d0815d011ef
Nico Welles <nico@welles.io>
2022-03-23 16:40:21 +0100

    Refactor given name getter

--- StammPhoenix.Web/Extensions/IdentityExtensions.cs
+++ StammPhoenix.Web/Extensions/IdentityExtensions.cs
@@ -7,7 +7,12 @@ public static class IdentityExtensions
 {
     public static string GetGivenName(this IIdentity? identity)
     {
-        var claimsIdentity = (ClaimsIdentity) identity;
+        var claimsIdentity = identity as ClaimsIdentity;
+
+        if (claimsIdentity == null)
+        {
+            throw new ArgumentNullException(nameof(identity));
+        }
 
         return claimsIdentity.Claims.Single(x => x.Type == ClaimTypes.GivenName).Value;
     }
