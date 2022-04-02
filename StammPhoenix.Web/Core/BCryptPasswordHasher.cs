92fb3388e8d08a92335eb84da99a3b58f6a7ec6f
Nico Welles <nico@welles.io>
2022-04-02 02:14:03 +0200

    [TEMP] Fixed password salt test

--- StammPhoenix.Web/Core/BCryptPasswordHasher.cs
+++ StammPhoenix.Web/Core/BCryptPasswordHasher.cs
@@ -2,21 +2,22 @@
 
 public class BCryptPasswordHasher : IPasswordHasher
 {
-    private const int WorkLoad = 12;
+    //private const int WorkLoad = 12;
+    private const string Salt = "$2a$12$/v4WQkKf4MH8AUma.6eLnO";
 
     public string HashPassword(string password)
     {
-        return BCrypt.Net.BCrypt.HashPassword(password, WorkLoad);
+        return BCrypt.Net.BCrypt.HashPassword(password, BCryptPasswordHasher.Salt);
     }
 
     public PasswordVerificationResult VerifyHashedPassword(string hashedPassword, string providedPassword)
     {
         var isValid = BCrypt.Net.BCrypt.Verify(providedPassword, hashedPassword);
 
-        if (isValid && BCrypt.Net.BCrypt.PasswordNeedsRehash(hashedPassword, WorkLoad))
-        {
-            return PasswordVerificationResult.SuccessRehashNeeded;
-        }
+        // if (isValid && BCrypt.Net.BCrypt.PasswordNeedsRehash(hashedPassword, WorkLoad))
+        // {
+        //     return PasswordVerificationResult.SuccessRehashNeeded;
+        // }
 
         return isValid ? PasswordVerificationResult.Success : PasswordVerificationResult.Failed;
     }
