8f8380508a46494d6a6a1fb8c650d010695b9b78
Nico Welles <nico@welles.io>
2022-04-02 12:46:14 +0200

    Do not allow setting ID outside of Persistence context

--- StammPhoenix.Persistence/Models/LoginUser.cs
+++ StammPhoenix.Persistence/Models/LoginUser.cs
@@ -8,7 +8,7 @@ namespace StammPhoenix.Persistence.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public Guid Id { get; set; }
+        public Guid Id { get; internal set; }
 
         public string Name { get; set; }
 
--- StammPhoenix.Persistence/Models/PageContact.cs
+++ StammPhoenix.Persistence/Models/PageContact.cs
@@ -7,7 +7,7 @@ public class PageContact
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public Guid Id { get; set; }
+    public Guid Id { get; internal set; }
 
     public string Name { get; set; }
 
