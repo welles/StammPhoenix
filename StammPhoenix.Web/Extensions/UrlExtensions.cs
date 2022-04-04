91d1c65136effa27acb482b78f1d181e1ab20150
Nico Welles <nico@welles.io>
2022-04-04 23:20:12 +0200

    Add dynamic route values to Url extensions

--- StammPhoenix.Web/Extensions/UrlExtensions.cs
+++ StammPhoenix.Web/Extensions/UrlExtensions.cs
@@ -7,7 +7,8 @@ namespace StammPhoenix.Web.Extensions;
 public static class UrlExtensions
 {
     public static string? To(this IUrlHelper urlHelper, [AspMvcAction] string? action,
-        [AspMvcController] string? controller, [AspMvcArea] string? area = null, string? redirect = null)
+        [AspMvcController] string? controller, [AspMvcArea] string? area = null, string? redirect = null,
+        dynamic? routeValues = null)
     {
         dynamic routeValuesResult = new ExpandoObject();
         routeValuesResult.area = area ?? string.Empty;
@@ -15,18 +16,35 @@ public static class UrlExtensions
         {
             routeValuesResult.redirect = redirect;
         }
+        if (routeValues != null)
+        {
+            routeValues = new RouteValueDictionary(routeValues);
+            foreach (var entry in routeValues)
+            {
+                ((IDictionary<string, object>)routeValuesResult).Add(entry.Key, entry.Value);
+            }
+        }
 
         return urlHelper.Action(action, controller, (object)routeValuesResult);
     }
 
     public static IActionResult RedirectTo(this ControllerBase controllerBase, [AspMvcAction] string? action,
-        [AspMvcController] string? controller, [AspMvcArea] string? area = null, string? redirect = null)
+        [AspMvcController] string? controller, [AspMvcArea] string? area = null, string? redirect = null,
+        dynamic? routeValues = null)
     {
         dynamic routeValuesResult = new ExpandoObject();
         routeValuesResult.area = area ?? string.Empty;
         if (redirect != null)
         {
             routeValuesResult.redirect = redirect;
+        }
+        if (routeValues != null)
+        {
+            routeValues = new RouteValueDictionary(routeValues);
+            foreach (var entry in routeValues)
+            {
+                ((IDictionary<string, object>)routeValuesResult).Add(entry.Key, entry.Value);
+            }
         }
 
         return controllerBase.RedirectToAction(action, controller, (object)routeValuesResult);
