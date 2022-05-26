f49d21e6b0c9ce14742fc867fc4b90498687c837
Nico Welles <nico@welles.io>
2022-05-26 22:00:07 +0200

    Change background color by clicking on icon

--- StammPhoenix.Web/wwwroot/js/site.js
+++ StammPhoenix.Web/wwwroot/js/site.js
@@ -3,10 +3,15 @@
 */
 
 $(document).ready(function () {
-    const backgroundColor = sessionStorage.getItem('background-color')
-        ?? "hsl(" + 360 * Math.random() + ',' + (25 + 70 * Math.random()) + '%,' + (80 + 10 * Math.random()) + '%)';
+    updateBackgroundColor(sessionStorage.getItem('background-color'));
+
+    $('.navbar-brand').click(_ => sessionStorage.removeItem('background-color'));
+});
+
+function updateBackgroundColor(cacheColor) {
+    const backgroundColor = cacheColor ?? "hsl(" + 360 * Math.random() + ',' + (25 + 70 * Math.random()) + '%,' + (80 + 10 * Math.random()) + '%)';
 
     sessionStorage.setItem('background-color', backgroundColor);
 
     $('body').css('background-color', backgroundColor);
-});
+}
