2891b245bb72dd2994ec978f1e2a0819214c92c5
Nico Welles <nico@welles.io>
2022-05-26 14:46:08 +0200

    Get pastel background color and save to session

--- StammPhoenix.Web/wwwroot/js/site.js
+++ StammPhoenix.Web/wwwroot/js/site.js
@@ -1,3 +1,12 @@
 ﻿/*!
 * Site Specific JS
 */
+
+$(document).ready(function () {
+    const backgroundColor = sessionStorage.getItem('background-color')
+        ?? "hsl(" + 360 * Math.random() + ',' + (25 + 70 * Math.random()) + '%,' + (80 + 10 * Math.random()) + '%)';
+
+    sessionStorage.setItem('background-color', backgroundColor);
+
+    $('body').css('background-color', backgroundColor);
+});
