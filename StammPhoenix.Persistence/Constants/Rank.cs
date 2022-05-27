a71b11d49eecfcf5ecf8a4520da4927d5abd226b
Nico Welles <nico@welles.io>
2022-05-27 11:18:32 +0200

    Extend ranks

--- StammPhoenix.Persistence/Constants/Rank.cs
+++ StammPhoenix.Persistence/Constants/Rank.cs
@@ -3,11 +3,13 @@
     [Flags]
     public enum Rank
     {
-        Woelfling = 1 << 0,
+        Woelflinge = 1 << 0,
         Jungpfadfinder = 1 << 1,
         Pfadfinder = 1 << 2,
         Rover = 1 << 3,
         Leiter = 1 << 4,
-        Extern = 1 << 5
+        Extern = 1 << 5,
+        Ehemalige = 1 << 6,
+        Verein = 1 << 7
     }
 }
