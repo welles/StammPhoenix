e6c26c9c5797e07b5ea1b5518b8774d6a7c7d55d
Nico Welles <nico@welles.io>
2022-05-27 12:16:27 +0200

    Add extension for rank display name

---
+++ StammPhoenix.Persistence/Extensions/RankExtensions.cs
@@ -0,0 +1,22 @@
+﻿using StammPhoenix.Persistence.Constants;
+
+namespace StammPhoenix.Persistence.Extensions;
+
+public static class RankExtensions
+{
+    public static string ToDisplayName(this Rank rank, bool plural = false)
+    {
+        return rank switch
+        {
+            Rank.Woelflinge => plural ? "Wölflinge" : "Wölfling",
+            Rank.Jungpfadfinder => plural ? "Jungpfadfinder" : "Jungpfadfinder:in",
+            Rank.Pfadfinder => plural ? "Pfadfinder" : "Pfadfinder:in",
+            Rank.Rover => plural ? "Rover" : "Rover:in",
+            Rank.Leiter => plural ? "Leitende" : "Leiter:in",
+            Rank.Extern => plural ? "Externe" : "Externe:r",
+            Rank.Ehemalige => plural ? "Ehemalige" : "Ehemalige:r",
+            Rank.Verein => plural ? "Vereinsmitglieder" : "Vereinsmitglied",
+            _ => throw new ArgumentOutOfRangeException(nameof(rank), rank, null)
+        };
+    }
+}
