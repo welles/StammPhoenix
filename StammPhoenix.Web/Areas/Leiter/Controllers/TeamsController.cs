77ffd5da3d079b7d409f3d5a3231d0d901b866eb
Nico Welles <nico@welles.io>
2022-06-27 00:26:32 +0200

    Add basic Teams overview page

---
+++ StammPhoenix.Web/Areas/Leiter/Controllers/TeamsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StammPhoenix.Web.Areas.Leiter.Controllers;
+
+[Authorize]
+[Area("Leiter")]
+public class TeamsController : Controller
+{
+    [HttpGet]
+    [Authorize]
+    public IActionResult Index()
+    {
+        return this.View();
+    }
+}
